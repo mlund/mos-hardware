5bf50de556f39ae53d10a0002926bd66db2eceea
Mikael Lund <Mlund@mac.com>
2022-12-13 14:46:54 +0000

    Remove unsafe blocks from mega65-libc example

--- examples/mega65-libc.rs
+++ examples/mega65-libc.rs
@@ -1,10 +1,17 @@
 //! MEGA65 libc example
 //!
 //! This shows how to use mega65-libc which is exposed to rust using bindgen.
-//! As you may notice, the interface is often not idiomatic and several functions
-//! should be manually wrapped in future releases.
+//! A limited, but growing number of these are wrapped in _safe_ rust functions
+//! found in `mega65::`. 
 //!
-//! TODO: gotoxy() doesn't seem to work (at least not in xemu)
+//! ## TODO
+//! As of writing these functions do not seem to work as expected - at least not in xemu.
+//! Likely text manipulation is incompatible with rusts `print` macros and should instead
+//! be used with the functions found in `libc`.
+//! - `goto_xy()`
+//! - `go_home()`
+//! - `set_text_color()`
+//! - `get_real_time_clock()`
 
 #![no_std]
 #![feature(start)]
@@ -14,44 +21,39 @@ use core::panic::PanicInfo;
 use mos_hardware::mega65::*;
 use ufmt_stdio::*;
 
+#[start]
+fn _main(_argc: isize, _argv: *const *const u8) -> isize {
+    conio_init();
+    clear_screen();
+    go_home();
+    goto_xy(1, 1);
+    set_upper_case();
+
+    let resolution = get_screen_size();
+    println!("SCREEN SIZE = {} x {}", resolution.width, resolution.height);
+
+    print!("RANDUM BYTES FROM LIBC: ");
+    for _ in 0..10 {
+        print!("{} ", rand8(u8::MAX));
+    }
+    print!("\nRANDUM BYTES FROM SID:  ");
+    sid0().start_random_generator();
+    for _ in 0..10 {
+        print!("{} ", sid0().rand8(u8::MAX));
+    }
+    println!();
+
+    set_border_color(libc::COLOUR_BROWN as u8);
+    set_text_color(libc::COLOUR_BLACK as u8);
+
+    let rtc = get_real_time_clock();
+    println!("TIME = {}:{}:{}", rtc.tm_hour, rtc.tm_min, rtc.tm_sec);
+    0
+}
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     #[cfg(not(target_vendor = "nes-nrom-128"))]
-    print!("!");
+    print!("PANIC!");
     loop {}
-}
-
-#[start]
-fn _main(_argc: isize, _argv: *const *const u8) -> isize {
-    unsafe {
-        // play with screen
-        libc::conioinit();
-        libc::clrscr();
-        libc::gohome();
-        libc::gotoxy(1, 1);
-        let mut width: u8 = 0;
-        let mut height: u8 = 0;
-        libc::getscreensize(&mut width, &mut height);
-        println!("SCREEN SIZE = {} x {}", width, height);
-
-        // random numbers
-        println!("RANDUM BYTES FROM SID:");
-        (*SID0).start_random_generator();
-        for _i in 0..3 {
-            println!("{}", (*SID0).rand8(u8::MAX));
-        }
-        println!("RANDUM BYTES FROM LIBC:");
-        for _i in 0..3 {
-            println!("{}", libc::rand8(u8::MAX));
-        }
-
-        // play with colors
-        libc::bordercolor(libc::COLOUR_BROWN as u8);
-
-        // real-time-clock
-        let mut rtc = libc::m65_tm::default();
-        libc::getrtc(&mut rtc);
-        println!("TIME = {}:{}:{}", rtc.tm_hour, rtc.tm_min, rtc.tm_sec);
-    }
-    0
 }
