be2e61ac746c3d945131280040b025f0e133ea28
Mikael Lund <Mlund@mac.com>
2022-12-27 09:21:52 +0000

    Update mega65 plasma example

--- examples/mega65-plasma.rs
+++ examples/mega65-plasma.rs
@@ -1,4 +1,4 @@
-//! C64 Plasma Example (80 x 25 mode for mega65)
+//! Plasma Example (80 x 25 mode for mega65)
 //!
 //! - (w)2001 by groepaz; sourced from the CC65 /samples/cbm directory
 //! - Cleanup and porting to CC65 by Ullrich von Bassewitz.
@@ -8,92 +8,110 @@
 #![feature(start)]
 #![feature(default_alloc_error_handler)]
 
+extern crate mos_hardware;
+extern crate ufmt_stdio;
+
 use core::panic::PanicInfo;
-use itertools::iproduct;
-use mos_hardware::*;
+use mos_hardware::{mega65, repeat_element, sine, vic2, SINETABLE};
 use ufmt_stdio::*;
 
-/// Generate stochastic character set
-unsafe fn make_charset(charset_ptr: *mut u8) {
-    const BITS: [u8; 8] = [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80];
+/// Class for rendering a character mode plasma effect
+struct Plasma {
+    yindex1: u8,
+    yindex2: u8,
+    xindex1: u8,
+    xindex2: u8,
+    xbuffer: [u8; 80],
+    ybuffer: [u8; 25],
+}
 
-    repeat_element(mos_hardware::SINETABLE.iter().copied(), 8)
-        .enumerate()
-        .for_each(|(cnt, sine)| {
-            let mut char_pattern = 0b00000000u8;
+impl Plasma {
+    /// Create new instance and initialize character set
+    pub fn new(charset_address: *mut u8) -> Plasma {
+        Plasma::make_charset(charset_address);
+        Plasma {
+            yindex1: 0,
+            yindex2: 0,
+            xindex1: 0,
+            xindex2: 0,
+            xbuffer: [0; 80],
+            ybuffer: [0; 25],
+        }
+    }
+    /// Generate stochastic character set
+    pub fn make_charset(charset_address: *mut u8) {
+        let generate_char = |sine| {
+            const BITS: [u8; 8] = [1, 2, 4, 8, 16, 32, 64, 128];
+            let mut char_pattern: u8 = 0;
             BITS.iter()
-                .filter(|_| mega65::libc::rand8(u8::MAX) > sine)
+                .filter(|_| mega65::rand8(u8::MAX) > sine)
                 .for_each(|bit| {
                     char_pattern |= bit;
                 });
-            poke!(charset_ptr.offset(cnt as isize), char_pattern);
-            if cnt % 64 == 0 {
-                print!(".");
+            char_pattern
+        };
+
+        repeat_element(SINETABLE.iter().copied(), 8)
+            .enumerate()
+            .for_each(|(cnt, sine)| {
+                let character = generate_char(sine);
+                unsafe {
+                    charset_address.add(cnt).write_volatile(character);
+                }
+                if cnt % 64 == 0 {
+                    print!(".");
+                }
+            });
+    }
+
+    /// Render entire screen at given address
+    pub fn render(&mut self, screen_address: *mut u8) {
+        let mut i = self.yindex1;
+        let mut j = self.yindex2;
+        for y in self.ybuffer.iter_mut() {
+            *y = sine(i).wrapping_add(sine(j));
+            i = i.wrapping_add(4);
+            j = j.wrapping_add(9);
+        }
+        self.yindex1 = self.yindex1.wrapping_add(3);
+        self.yindex2 = self.yindex2.wrapping_sub(5);
+
+        i = self.xindex1;
+        j = self.xindex2;
+        for x in self.xbuffer.iter_mut() {
+            *x = sine(i).wrapping_add(sine(j));
+            i = i.wrapping_add(3);
+            j = j.wrapping_add(7);
+        }
+        self.xindex1 = self.xindex1.wrapping_add(2);
+        self.xindex2 = self.xindex2.wrapping_sub(3);
+
+        let mut offset: usize = 0; // screen memory offset
+        for y in self.ybuffer.iter().copied() {
+            for x in self.xbuffer.iter().copied() {
+                let sum = x.wrapping_add(y);
+                unsafe {
+                    screen_address.add(offset).write_volatile(sum);
+                }
+                offset += 1;
             }
-        });
-}
-
-/// Render entire screen
-/// @todo Rename to meaningful variable names (reminiscence from C)
-unsafe fn render_plasma(screen_ptr: *mut u8) {
-    static mut C1A: u8 = 0;
-    static mut C1B: u8 = 0;
-    static mut C2A: u8 = 0;
-    static mut C2B: u8 = 0;
-    static mut XBUF: [u8; 80] = [0; 80];
-    static mut YBUF: [u8; 25] = [0; 25];
-
-    let mut c1a = C1A;
-    let mut c1b = C1B;
-    YBUF.iter_mut().for_each(|y| {
-        *y = add!(
-            mos_hardware::SINETABLE[c1a as usize],
-            mos_hardware::SINETABLE[c1b as usize]
-        );
-        c1a = add!(c1a, 4);
-        c1b = add!(c1b, 9);
-    });
-    C1A = add!(C1A, 3);
-    C1B = sub!(C1B, 5);
-
-    let mut c2a = C2A;
-    let mut c2b = C2B;
-    XBUF.iter_mut().for_each(|x| {
-        *x = add!(
-            mos_hardware::SINETABLE[c2a as usize],
-            mos_hardware::SINETABLE[c2b as usize]
-        );
-        c2a = add!(c2a, 3);
-        c2b = add!(c2b, 7);
-    });
-    C2A = add!(C2A, 2);
-    C2B = sub!(C2B, 3);
-
-    iproduct!(YBUF.iter().copied(), XBUF.iter().copied())
-        .enumerate()
-        .for_each(|(cnt, (y, x))| {
-            poke!(screen_ptr.offset(cnt as isize), add!(y, x));
-        })
+        }
+    }
 }
 
 #[start]
 fn _main(_argc: isize, _argv: *const *const u8) -> isize {
-    mega65::speed_mode3(); // Set CPU speed to 3.5 Mhz
-    const CHARSET: u16 = 0x3000; // Custom charset
-    const SCREEN1: u16 = 0x0800; // Set up two character screens...
-    const SCREEN2: u16 = 0x2800; // ...for double buffering
-    const PAGE1: u8 =
-        vic2::ScreenBank::from_address(SCREEN1).bits() | vic2::CharsetBank::from(CHARSET).bits();
-    const PAGE2: u8 =
-        vic2::ScreenBank::from_address(SCREEN2).bits() | vic2::CharsetBank::from(CHARSET).bits();
-    unsafe {
-        make_charset(CHARSET as *mut u8);
-        loop {
-            render_plasma(SCREEN1 as *mut u8);
-            (*mega65::VICII).screen_and_charset_bank.write(PAGE1);
-            render_plasma(SCREEN2 as *mut u8);
-            (*mega65::VICII).screen_and_charset_bank.write(PAGE2);
-        }
+    const CHARSET: u16 = 0x3000; // Custom character set location
+    let page = vic2::ScreenBank::from_address(mega65::DEFAULT_SCREEN as u16).bits()
+        | vic2::CharsetBank::from(CHARSET).bits();
+
+    let mut plasma = Plasma::new(CHARSET as *mut u8);
+
+    unsafe { (*mega65::VICII).screen_and_charset_bank.write(page) };
+
+    mega65::speed_mode3(); // reduce CPU speed to 3.5 Mhz
+    loop {
+        plasma.render(mega65::DEFAULT_SCREEN);
     }
 }
 
